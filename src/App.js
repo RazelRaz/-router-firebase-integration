63afd173f1778a93e6b6e70df3a929f0770c9c36
RazelRaz <razel16dcm@gmail.com>
2022-04-13 21:57:04 +0600

    final done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Register from './components/Register/Register';
+import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -13,6 +17,12 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/products' element={<Products></Products>}>Products</Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        }></Route>
       </Routes>
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,19 +4,18 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
 // import useFirebase from '../../hooks/useFirebase';
 
-const auth = getAuth(app)
+const  auth = getAuth(app)
 
-const Home = () => {
-    // const {user} = useFirebase();
+const Products = () => {
+    // const {user} = useFirebase(); 
 
     const [user] = useAuthState(auth)
-
     return (
         <div>
-            <h2>This is Home</h2>
+            <h2>This is Product</h2>
             <p>Current User: {user? user.displayName : 'Nobody'}</p>
         </div>
     );
 };
 
-export default Home;
+export default Products;
